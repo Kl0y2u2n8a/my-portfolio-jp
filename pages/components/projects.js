2c1acad1c2af4c8e03a0ee9d77af124792d1dcd2
Kl0y2u2n8a <kevinliang228@gmail.com>
2024-03-14 16:58:42 +0900

    update project

--- pages/components/projects.js
+++ pages/components/projects.js
@@ -21,7 +21,7 @@ function MyProject(props, projectRef) {
                         <p className="md:max-w-lg p-2 font-rony9 bg-gradient-to-r from-fuchsia-500 dark:from-sky-400 via-rose-400 dark:via-indigo-100 to-red-500 dark:to-red-400  bg-clip-text text-transparent">
                             これはTypescriptとReactのミニプロジェクトです。
                             自分で自主学習のため、作成しました。
-                            下のフォームを入力すると上のボックスにユーザーさんが追加した内容を表示します。
+                            上のフォームを入力すると下のボックスにユーザーさんの入金か振込の内容を表示します。
                             </p>
                         <p className="md:max-w-lg py-2 font-rony9 bg-gradient-to-r from-fuchsia-500 dark:from-sky-400 via-rose-400 dark:via-indigo-100 to-red-500 dark:to-red-400  bg-clip-text text-transparent">* 未完成、作成中 *</p>
 
@@ -32,13 +32,13 @@ function MyProject(props, projectRef) {
                         <button className="mt-3 rounded-lg border border-gray-800 p-1 text-sm text-blue-700 font-burtons 
                             bg-gradient-to-r from-blue-400 via-white to-sky-500 hover:from-sky-500 hover:via-white hover:to-blue-400 hover:text-violet-500 
                             dark:from-blue-600 dark:to-sky-600 dark:text-white dark:hover:from-sky-600 dark:hover:via-gray-400 dark:hover:to-blue-600">
-                            <a href="https://github.com/Kl0y2u2n8a/HelloWorld" target="_blank">Resource Code</a>
+                            <a href="https://github.com/Kl0y2u2n8a/financelog" target="_blank">Resource Code</a>
                         </button>
 
                         <button className="mt-3 mx-3 rounded-lg border border-gray-800 p-1 text-sm text-blue-700 font-burtons bg-gradient-to-r 
                             from-blue-400 via-white to-sky-500 hover:from-sky-500 hover:via-white hover:to-blue-400 hover:text-violet-500 
                             dark:from-blue-600 dark:to-sky-600 dark:text-white dark:hover:from-sky-600 dark:hover:via-gray-400 dark:hover:to-blue-600">
-                                <a href="https://hello-world-qsdmh6h2j-kl0y2u2n8as-projects.vercel.app/" target="_blank">Website</a>
+                                <a href="https://financelog-zeta.vercel.app/" target="_blank">Website</a>
                         </button>
 
                     </div>
